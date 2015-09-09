c3decc09c74bce60ad0e9da9a8a8478bfe786329
Eaco <benjaminmjmorris@gmail.com>
2015-09-08 22:31:31 -0400

    Initial commit
    A semi working web application, lets see if the Database handles it....

---
+++ app/socket.js
@@ -0,0 +1,88 @@
+/**
+ * Created by Ben on 8/24/2015.
+ */
+var Room            = require('../app/models/room');
+var User            = require('../app/models/user');
+
+module.exports = function(io){
+    io.sockets.on('connection', function(socket){
+        console.log('connected!!');
+
+        socket.on('here', function(user){
+            socket.to(socket.room).broadcast.emit('here', user);
+        });
+
+        socket.on('message', function(msg){
+            Room.findOne({name: socket.room.name}, function(err, curroom) {
+                if (curroom){
+                    if(curroom.messages == null)
+                        curroom.messages =[];
+                    console.log("Pushing " + msg);
+                    curroom.messages.push(msg);
+                    curroom.save(function(err){
+                        console.log('room updated successfully');
+                    });
+                    socket.room = curroom;
+                    console.log(curroom.messages);
+                }
+                else{
+                    curroom.messages = [];
+                    curroom.messages.push(msg);
+                    socket.room = curroom;
+                }
+//                console.log(socket.room)
+            });
+            io.to(socket.room.name).emit('message', msg);
+        });
+        socket.on('clear', function () {
+            io.to(socket.room).emit('clearing');
+        });
+        socket.on('poster', function(post){
+            console.log('Posting...' + post.image);
+
+        });
+        socket.on('characterAdded', function(char){
+            Room.findOne({name: socket.room.name}, function(err, curroom) {
+                if (curroom){
+                    curroom.characters.push(char);
+                    curroom.save(function(err){
+                        console.log('room updated successfully');
+                    });
+                    socket.room = curroom;
+                }
+                else{
+                    curroom.characters = [];
+                    curroom.characters.push(char);
+                    socket.room = curroom;
+                }
+                console.log(socket.room)
+            });
+        });
+
+        socket.on('roomChange', function(room, fn){
+            socket.leave(socket.room);
+            Room.findOne({name: room}, function(err, curroom){
+                if(curroom){
+                    console.log("found the room");
+                    //.currentroom =    curroom;
+                }
+                else{
+                    var newRoom = Room({name:room});
+                    newRoom.save(function(err){
+                        if(err) throw err;
+
+                        console.log("new room created: " + room);
+                    });
+                    curroom = newRoom;
+                }
+
+                socket.room = curroom;
+                socket.join(curroom.name);
+                console.log(socket.room.name)
+                console.log(curroom.characters);
+                fn(curroom);                        //HOLY CRAP THIS MAKES NO SENSE AT ALL
+            });
+            //console.log('gone to room: ' + room);
+        });
+    })
+};
---
+++ config/database.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Ben on 8/24/2015.
+ */
+module.exports = {
+
+  'url' : 'mongodb://localhost/test'
+};
